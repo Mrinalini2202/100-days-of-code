56ffc428140f40c8b13bb668c5d0784110b6751c
Mrinalini2202 <mrinalinisharma2202@gmail.com>
2025-11-27 16:40:48 +0530

    Update 590023457-Mrinalini-005-Q9.c

--- 590023457-Mrinalini-005-Q9.c
+++ 590023457-Mrinalini-005-Q9.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-#include <math.h>
+#include <math.h> 
 
 int main() {
-    double principal, rate, time;
-    double simpleInterest, compoundInterest;
+    float principal, rate, time;
+    float simple_interest, compound_interest, amount;
 
-    
-    printf("Enter principal amount: ");
-    scanf("%lf", &principal);
-    printf("Enter rate of interest (in %%): ");
-    scanf("%lf", &rate);
-    printf("Enter time (in years): ");
-    scanf("%lf", &time);
+    printf("Enter Principal amount: ");
+    scanf("%f", &principal);
+    printf("Enter Rate of interest (in %%): ");
+    scanf("%f", &rate);
+    printf("Enter Time (in years): ");
+    scanf("%f", &time);
 
-    
-    simpleInterest = (principal * rate * time) / 100;
+    simple_interest = (principal * rate * time) / 100;
 
-    
-    compoundInterest = principal * pow(1 + rate / 100, time) - principal;
+    amount = principal * pow((1 + rate / 100), time);
+    compound_interest = amount - principal;
 
-   
-    printf("\nSimple Interest = %.2lf\n", simpleInterest);
-    printf("Compound Interest = %.2lf\n", compoundInterest);
+    printf("\nSimple Interest = %.2f\n", simple_interest);
+    printf("Compound Interest = %.2f\n", compound_interest);
 
     return 0;
 }
+
