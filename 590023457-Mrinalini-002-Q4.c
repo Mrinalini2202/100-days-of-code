15e7a487067106ea39a90362a08722da91f3cfd4
Mrinalini2202 <mrinalinisharma2202@gmail.com>
2025-11-27 16:38:06 +0530

    Update 590023457-Mrinalini-002-Q4.c

--- 590023457-Mrinalini-002-Q4.c
+++ 590023457-Mrinalini-002-Q4.c
@@ -4,17 +4,15 @@
 int main() {
     float radius, area, circumference;
 
-    
     printf("Enter the radius of the circle: ");
     scanf("%f", &radius);
 
-    
     area = PI * radius * radius;
     circumference = 2 * PI * radius;
 
-    
-    printf("\nArea of the circle = %.2f", area);
-    printf("\nCircumference of the circle = %.2f\n", circumference);
+    printf("Area of the circle = %.2f\n", area);
+    printf("Circumference of the circle = %.2f\n", circumference);
 
     return 0;
 }
+
