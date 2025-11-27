1f9a9f9e6653778f3278aba46589043168c11152
Mrinalini2202 <mrinalinisharma2202@gmail.com>
2025-11-27 16:43:57 +0530

    Update 590023457-Mrinalini-007-Q13.c

--- 590023457-Mrinalini-007-Q13.c
+++ 590023457-Mrinalini-007-Q13.c
@@ -2,17 +2,19 @@
 
 int main() {
     int year;
-
-    
     printf("Enter a year: ");
     scanf("%d", &year);
 
-    
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-        printf("%d is a leap year.\n", year);
+    if (year % 400 == 0) {
+        printf("%d is a Leap Year\n", year);
+    } else if (year % 100 == 0) {
+        printf("%d is Not a Leap Year\n", year);
+    } else if (year % 4 == 0) {
+        printf("%d is a Leap Year\n", year);
     } else {
-        printf("%d is not a leap year.\n", year);
+        printf("%d is Not a Leap Year\n", year);
     }
 
     return 0;
 }
+
