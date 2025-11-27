f5fec32e20e6858b7058812b927bf11d34828a96
Mrinalini2202 <mrinalinisharma2202@gmail.com>
2025-11-27 16:43:02 +0530

    Update 590023457-Mrinalini-006-Q11.c

--- 590023457-Mrinalini-006-Q11.c
+++ 590023457-Mrinalini-006-Q11.c
@@ -3,15 +3,13 @@
 int main() {
     int num;
 
-    
     printf("Enter an integer: ");
     scanf("%d", &num);
 
-   
     if (num % 2 == 0) {
-        printf("%d is even.\n", num);
+        printf("%d is Even\n", num);
     } else {
-        printf("%d is odd.\n", num);
+        printf("%d is Odd\n", num);
     }
 
     return 0;
