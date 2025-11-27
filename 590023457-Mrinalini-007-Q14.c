52a2004fa5d532cf59c6557d30388ad4fe02dabc
Mrinalini2202 <mrinalinisharma2202@gmail.com>
2025-11-27 16:44:17 +0530

    Update 590023457-Mrinalini-007-Q14.c

--- 590023457-Mrinalini-007-Q14.c
+++ 590023457-Mrinalini-007-Q14.c
@@ -2,18 +2,16 @@
 
 int main() {
     char ch;
-
-    
     printf("Enter a character: ");
     scanf("%c", &ch);
 
-    
     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
         ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-        printf("%c is a vowel.\n", ch);
+        printf("%c is a Vowel\n", ch);
     } else {
-        printf("%c is a consonant.\n", ch);
+        printf("%c is a Consonant\n", ch);
     }
 
     return 0;
 }
+
