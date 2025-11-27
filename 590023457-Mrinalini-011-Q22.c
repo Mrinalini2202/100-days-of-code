38de6bbeb5173464462be65e0527da626cb64444
Mrinalini2202 <mrinalinisharma2202@gmail.com>
2025-11-27 17:04:32 +0530

    Create 590023457-Mrinalini-011-Q22.c

---
+++ 590023457-Mrinalini-011-Q22.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    float cp, sp, percent;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &cp);
+    printf("Enter Selling Price: ");
+    scanf("%f", &sp);
+
+    if(sp > cp) {
+        percent = ((sp - cp) / cp) * 100;
+        printf("Profit Percentage = %.2f%%\n", percent);
+    }
+    else if(cp > sp) {
+        percent = ((cp - sp) / cp) * 100;
+        printf("Loss Percentage = %.2f%%\n", percent);
+    }
+    else {
+        printf("No Profit No Loss\n");
+    }
+
+    return 0;
+}
