9960c0e4e65c0bf2df67a377a4afc96b8dd9264c
Mrinalini2202 <mrinalinisharma2202@gmail.com>
2025-11-27 16:42:31 +0530

    Update 590023457-Mrinalini-005-Q10.c

--- 590023457-Mrinalini-005-Q10.c
+++ 590023457-Mrinalini-005-Q10.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int totalSeconds, hours, minutes, seconds;
+    int total_seconds, hours, minutes, seconds;
 
-    
     printf("Enter time in seconds: ");
-    scanf("%d", &totalSeconds);
+    scanf("%d", &total_seconds);
 
-    
-    hours = totalSeconds / 3600;
-    minutes = (totalSeconds % 3600) / 60;
-    seconds = totalSeconds % 60;
+    hours = total_seconds / 3600;             
+    minutes = (total_seconds % 3600) / 60;    
+    seconds = total_seconds % 60;             
 
-    
-    printf("Time in HH:MM:SS format = %02d:%02d:%02d\n", hours, minutes, seconds);
+    printf("Time: %02d:%02d:%02d\n", hours, minutes, seconds);
 
     return 0;
 }
